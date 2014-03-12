98dbc79350283d35958bf18592423aa31b5d390b
golding <Arne.Gylling@Toorion.com>
2014-03-12 15:24:45 +0100

    V1.2

--- WiringBBB.h
+++ WiringBBB.h
@@ -20,6 +20,9 @@
 #define P8_17 8
 #define P8_20 9
 #define P8_21 10
+#define P9_12 11
+ 
+#define DEBUG_GPIO 0
 
 /** @brief Contains all elements which we need to pass into the beagleboard 
  * to specify which GPIO pin we want to turn ON/OFF **/ 
@@ -31,14 +34,22 @@ struct gpioID
 	char GPIOMUX[10];   //e.g: gpmc_ad6; 
 };
 
-void pulsePin(struct gpioID enabled_gpio[],unsigned int data_to_write,int nbr_selectedPins, int print_to_screen, const char *pinDescription[], int pinID, int delay);
-void write_GPIO_value(int GPIONUMBER, int value);
-void cleanup_GPIO(struct gpioID selected_GPIOs[],int selectedPins[], int nbr_selectedPins);
-void initialize_each_enabled_gpio(struct gpioID selected_GPIOs[],int selectedPins[], int nbr_selectedPins);
-void display_each_enabled_gpio(struct gpioID selected_GPIOs[],int nbr_selectedPins,const char *pinDescription[]);
-void turn_ON_OFF_pins(struct gpioID selected_GPIOs[],unsigned int data_to_write,int nbr_selectedPins, int print_to_screen, const char *pinDescription[]);
-//void turn_ON_OFF_pins(struct gpioID selected_GPIOs[],unsigned int data_to_write,int nbr_selectedPins, int print_to_screen);
 unsigned int bitWrite(unsigned int groupOfBits, int bitVal, int bitLoc);
 int bitRead(unsigned int groupOfBits, int bitLoc);
+
+void digitalWrite(struct gpioID singlePin,unsigned int data_to_write);
+void digitalWrite_multiple(struct gpioID selected_GPIOs[], int nbr_selectedPins, unsigned int data_to_write);
+void write_GPIO_value(int GPIONUMBER, int value);
+
+void pinMode_multiple(struct gpioID selected_GPIOs[],int selectedPins[], int nbr_selectedPins, const char *direction);
+void pinMode(struct gpioID *singlePin,int pinID, const char *direction);
+
+void cleanup(struct gpioID singlePin);
+void cleanup_multiple(struct gpioID selected_GPIOs[], int nbr_selectedPins);
+
+unsigned int digitalRead(struct gpioID singlePin);
+
+void delayms(unsigned long mseconds);
+void pulsePin(struct gpioID enabled_gpio[],unsigned int data_to_write,int nbr_selectedPins, int pinID, int delay);
 
 #endif
